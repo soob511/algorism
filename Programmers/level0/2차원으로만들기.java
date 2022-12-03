e02cc0517d0f6dee3f7815706b8f8ad7994b0bca
soob511 <102846772+soob511@users.noreply.github.com>
2022-12-03 21:25:13 +0900

    20221203

---
+++ Programmers/level0/2차원으로만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length/n][n];
+        // int c=0;
+        // for(int i=0;i<num_list.length/n;i++){
+        //     for(int j=0;j<n;j++){
+        //         answer[i][j] = num_list[c];
+        //         c++;
+        //     }
+        // }
+        // return answer;
+        
+        for(int i=0;i<num_list.length;i++){
+            answer[i/n][i%n] = num_list[i];
+        }
+        return answer;
+    }
+}
