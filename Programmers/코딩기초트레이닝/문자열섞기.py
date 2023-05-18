75f32866cec49d2b1ecd42d2f4158cfd4e4f9cf4
soob511 <102846772+soob511@users.noreply.github.com>
2023-05-18 23:35:14 +0900

    0518

---
+++ Programmers/코딩기초트레이닝/문자열섞기.py
@@ -0,0 +1,13 @@
+def solution(str1, str2):
+    answer = ''
+    for i,j in zip(str1,str2):
+        answer +=i
+        answer +=j
+    return answer
+  
+
+def solution(str1, str2):
+    answer = ''
+    for i in range(len(str1)):
+        answer+=str1[i]+str2[i]
+    return answer
