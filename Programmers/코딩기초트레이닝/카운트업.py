920c1050abe61893d3970aa9d8aacde723b1b3cc
soob511 <102846772+soob511@users.noreply.github.com>
2023-05-31 17:26:38 +0900

    0531

---
+++ Programmers/코딩기초트레이닝/카운트업.py
@@ -0,0 +1,9 @@
+def solution(start, end):
+    answer = []
+    for i in range(start,end+1):
+        answer.append(i)
+    return answer
+  
+  def solution(start, end):
+    
+    return list(range(start,end+1))
